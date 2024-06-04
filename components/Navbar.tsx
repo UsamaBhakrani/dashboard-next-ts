c8ddbeee69b0a73dd1767e9692b218dd5169b84e
Usama Bhakrani <usamabhakrani@gmail.com>
2024-06-04 22:12:52 +0500

    initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="bg-primary dark:bg-slate-700 py-2 px-5 flex justify-between text-white">
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
