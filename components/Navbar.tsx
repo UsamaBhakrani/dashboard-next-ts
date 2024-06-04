77a91d306e1c76575e1a638f43387a756736d6e9
Usama Bhakrani <usamabhakrani@gmail.com>
2024-06-04 23:40:53 +0500

    Sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,38 @@
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 const Navbar = () => {
   return (
-    <div className="bg-primary dark:bg-slate-700 py-2 px-5 flex justify-between text-white">
-      Navbar
+    <div className="bg-primary dark:bg-slate-700 py-2 px-5 flex justify-between items-center text-white">
+      <Link href="/">
+        <div>LOGO</div>
+      </Link>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="focus:outline-none">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href="/profile">Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href="/auth">Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
