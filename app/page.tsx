15a299165b71c1f0f0e5eca8a2f0bb8d9816dd42
Mohammed Usama <usamabhakrani@gmail.com>
2024-06-05 12:22:15 +0500

    PostsTable Finalized

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,33 @@
+import DashBoardCard from "@/components/dashboard/DashBoardCard";
+import PostsTable from "@/components/posts/PostsTable";
+import { Folders, List, Newspaper } from "lucide-react";
+
 export default function Home() {
-  return <main>Hello World</main>;
+  return (
+    <>
+      <div className="flex flex-col justify-between gap-5 mr-5 md:flex-row">
+        <DashBoardCard
+          icon={<List className="text-slate-500" size={72} />}
+          title="Orders"
+          count={100}
+        />
+        <DashBoardCard
+          icon={<Newspaper className="text-slate-500" size={72} />}
+          title="Products"
+          count={100}
+        />
+        <DashBoardCard
+          icon={<Folders className="text-slate-500" size={72} />}
+          title="Categories"
+          count={100}
+        />
+        <DashBoardCard
+          icon={<Newspaper className="text-slate-500" size={72} />}
+          title="Orders"
+          count={100}
+        />
+      </div>
+      <PostsTable title="Latest Posts" limit={5} />
+    </>
+  );
 }
---
+++ components/posts/PostsTable.tsx
@@ -0,0 +1,70 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import posts from "@/data/posts";
+import Link from "next/link";
+import { Post } from "@/types/posts";
+
+interface DataTableProps {
+  limit?: number;
+  title?: string;
+}
+
+const PostsTable = ({ limit, title }: DataTableProps) => {
+  const sortedPosts: Post[] = [...posts].sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+
+  const filteredPosts: Post[] = limit
+    ? sortedPosts.slice(0, limit)
+    : sortedPosts;
+
+  return (
+    <div className="mt-10">
+      <h3 className="text-2xl mb-4 font-semibold">{title ? title : "Posts"}</h3>
+      <Table>
+        <TableCaption>List of Recent Posts</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Title</TableHead>
+            <TableHead className="hidden md:table-cell">Author</TableHead>
+            <TableHead className="hidden md:table-cell text-right">
+              Date
+            </TableHead>
+            <TableHead className="hidden md:table-cell text-right">
+              View
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {filteredPosts.map(({ author, body, comments, date, id, title }) => {
+            return (
+              <TableRow key={id}>
+                <TableCell>{title}</TableCell>
+                <TableCell className="hidden md:table-cell">{author}</TableCell>
+                <TableCell className="hidden md:table-cell text-right">
+                  {date}
+                </TableCell>
+                <TableCell className="hidden md:table-cell text-right">
+                  <Link href={`/posts/edit/${id}`}>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs">
+                      Edit
+                    </button>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default PostsTable;
